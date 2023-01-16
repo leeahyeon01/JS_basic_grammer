237dae6278452c50868aaf74513c960ca9fcfb8f
leeahyeon <leeahyeon01@gmail.com>
2023-01-16 22:27:39 +0900

    g

--- modern-Javascript/4.object/4.1_object.js
+++ modern-Javascript/4.object/4.1_object.js
@@ -1,20 +1,15 @@
-//[4.1객체] 
+//[4.1 객체] 
+// 객체 = {}를 이용해 만듬 키(key):값(value)쌍으로 구성된 프로퍼티를 여러개 넣을 수 있다. 
+// 키엔 문자형✨ 값엔 모든 자료형이 허용된다. 
 
-//객체 = {}를 이용해 만듬, 키(key):값(value)쌍으로 구성된 프로퍼티를 여러개 넣을수 있다.
-//키엔 문자형, 값엔 모든 자료형이 허용된다.
+//객체 생성 두가지 방법  
+// 방법 1
+let user = new O(); 
 
-//객체생성 방법1
-let user = {
-age: 20,
-name: "이아연",
-height: "160", 
-"likes birds": true 
+//방법 2 
+let user2 = {
+    age: 20, 
+    name: "이아연",
+    height: 160, 
+    "like birds": true //복수의 단어를 키로 가질떈 " "해줌
 }
-
-//객체생성 방법2
-let user2  = new Object(); 
-
-//객체 접근 방법 
-
-console.log(user.age);
-console.log(user["likes birds"]) 
